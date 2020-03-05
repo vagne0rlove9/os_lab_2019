d59fc0afd87c0c82e08174cf3ab7438c46362f86
vagne0rlove9 <kirik-pos@mail.ru>
2020-03-05 16:40:38 +0000

    added 1 task

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
-
+#include "string.h"
+#include <stdio.h>
 void RevertString(char *str)
 {
-	// your code here
+	char *new_str;
+    int len = strlen(str) + 1;
+    printf("%d asdada",len);
+    // for(int i=0; i<strlen(str);i++, len--)
+    // {
+    //     new_str[i] = str[len];
+    // }
+    // printf("%s",str);
 }
 
