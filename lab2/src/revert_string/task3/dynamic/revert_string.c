79ba2c664bfe5e5cf761ac0728e71e4bcb2505c2
vagne0rlove9 <kirik-pos@mail.ru>
2020-03-05 19:13:54 +0000

    change task3

---
+++ lab2/src/revert_string/task3/dynamic/revert_string.c
@@ -0,0 +1,22 @@
+#include "revert_string.h"
+#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+extern void RevertString(char *str)
+{
+    int len = strlen(str)-1;
+    char *new_str = malloc(sizeof(char) * len);
+    int i;
+    for(i=0; i<strlen(str);i++, len--)
+    {
+        new_str[i] = str[len];
+        printf("%c %d %d\n",new_str[i], i, len);
+    }
+    for(i=0; i<strlen(str); i++)
+    {
+        str[i] = new_str[i];
+    }
+    //printf("%c  sd",new_str[1]);
+}
+
