5098b5c47a2f451c5165c427200b1ed9534d4672
vagne0rlove9 <kirik-pos@mail.ru>
2020-03-16 19:47:32 +0000

    added task1

--- lab3/src/utils.c
+++ lab3/src/utils.c
@@ -4,7 +4,10 @@
 #include <stdlib.h>
 void GenerateArray(int *array, unsigned int array_size, unsigned int seed) {
   srand(seed);
-  for (int i = 0; i < array_size; i++) {
+  int i;
+  for (i = 0; i < array_size; i++) {
     array[i] = rand();
+    printf("%d ", array[i]);
   }
+  printf("\n");
 }
